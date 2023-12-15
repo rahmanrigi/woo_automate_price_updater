cf1d351d32309b3c7eeda23361a60b4713f55929
rahman rigi <rahmanrigi66@gmail.com>
2023-12-15 07:59:45 +0330

    update all products prices

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 import smtplib
 import requests
 
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger_file_handler = logging.handlers.RotatingFileHandler(
@@ -48,10 +49,26 @@ headers = {
     "User-Agent": "My User Agent"  # replace with a valid user agent string
 }
 
+param = {
+    "per_page": 100
+}
+try:
+    response = requests.get(url=URL, headers=headers, params=param)
+    response.raise_for_status()
+    products = response.json()
+except Exception as error:
+    logger.info(f"unsuccessful request: {error}")
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=EMAIL_APP_PASSWORD)
+        connection.sendmail(from_addr=EMAIL,
+                        to_addrs=f"{RECEIVE_EMAIL}",
+                        msg=f"Subject:Woo Price update failed\n\nAccess to the products was fail. So, products price update failed. Error message: {error}")
+
 
 
 def price_finder():
-    global dollar_price, pond_price, euro_price, CURRENCY_URL
+    global dollar_price, pond_price, euro_price, try_turkey_price, aed_emirates_price, CURRENCY_URL
     url = CURRENCY_URL
     try:
         currencyـheaders = {
@@ -69,74 +86,70 @@ def price_finder():
             connection.sendmail(from_addr=EMAIL,
                                 to_addrs=f"{RECEIVE_EMAIL}",
                                 msg=f"Subject:Woo Price update failed\n\nconnection to {url} wasn't successful. So, products price update failed. Error message: {error}")
-         
+
 
     usd_max = soup.find(id="usdmax")
-    eur = soup.find(id="price_eur")
-    en_pond = soup.find(id="price_gbp")
-
     dollar = usd_max.text.replace(",", "")
     dollar_price = int(str(dollar)[:-1])
 
+    en_pond = soup.find(id="price_gbp")
     pond = en_pond.text.replace(",", "")
     pond_price = int(str(pond)[:-1])
 
+    eur = soup.find(id="price_eur")
     eur = eur.text.replace(",", "")
     euro_price = int(str(eur)[:-1])
+    
+    aed_emirates = soup.find(id="price_aed")
+    aed = aed_emirates.text.replace(",", "")
+    aed_emirates_price = int(str(aed)[:-1])
+    
+    try_turkey = soup.find(id="price_try")
+    try_tr = try_turkey.text.replace(",", "")
+    try_turkey_price = int(str(try_tr)[:-1])
 
 price_finder()
-
-
-
-giftcards = giftcards
 
 data = {'update': []
         }
 
-def calculate_prices(money_price):
-    global data
-    gifts = ps_gifts[giftcards]
-    
-    for gift in gifts:
-        card_value = gifts[gift]['value']
+def calculate_prices(price,value, p_id):
+    global data, rounded_product_today_price
 
-        card_price = (money_price * card_value) + 100000
-        rounded_card_price = round(card_price, -4)
+    product_today_price = (price * value) + 100000
+    rounded_product_today_price = round(product_today_price, -4)
 
-        # print(f"giftcard id: {gifts[gift]['id']}, giftcard value:{card_value}, card price: {card_price}")
+    product_price_data = {'id': p_id,
+                    'regular_price': rounded_product_today_price}
+
+    data["update"].append(product_price_data)
         
-        product_id = gifts[gift]['id']
-        
-        updated_data = {'id': product_id,
-                        'regular_price': rounded_card_price}
-
-        data["update"].append(updated_data)
-        
-ps_gifts = giftcards['playstation']
-
-#Guide 
-# update other tip of products like below and above
-# it's for future more products 
-# apple_gifts = giftcards['apple']
-# and then add your condition like below
-# finally call calculate_price() and pass
-# related price on it
-
-for giftcards in ps_gifts:
-    if giftcards == "usa": 
-        calculate_prices(dollar_price)     
-    if giftcards == "emirates":
-        calculate_prices(dollar_price)
-    if giftcards == "england":
-        calculate_prices(pond_price)
-    if giftcards == "germany":
-        calculate_prices(euro_price)
+for product in products:
+    product_name = product['name']
+    product_id = product['id']
+    product_yesterday_price = product['price']
+    product_attributes = product['attributes']
+    for attribute in product_attributes:
+        if attribute['id'] == 5:
+            currency_type = attribute['options'][0]
+        if attribute['id'] == 3:
+            product_value = int(attribute['options'][0])
+    if currency_type == "DOLLAR":
+        calculate_prices(dollar_price,product_value,product_id)
+    if currency_type == "TRY":
+        calculate_prices(try_turkey_price, product_value, product_id)
+    if currency_type == "AED":
+        calculate_prices(aed_emirates_price, product_value, product_id)
+    if currency_type == "EURO":
+        calculate_prices(euro_price, product_value, product_id)
+    if currency_type == "POUND":
+        calculate_prices(pond_price, product_value, product_id)
 
 
 if __name__ == "__main__":
     # logger.info(f"Token value: {SOME_SECRET}")        
     try:
-        woo_response = requests.post(BATCH_PRODUCTS_UPDATE_URL, headers=headers, json= data)
+        woo_response = requests.post(BATCH_PRODUCTS_UPDATE_URL, headers=headers, json=data)
         if woo_response.status_code == 200:
             with smtplib.SMTP("smtp.gmail.com") as connection:
                 connection.starttls()
