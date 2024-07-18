c33d2beb90ef7ada847f95e0e93eaa78f43fffa6
rahman rigi <rahmanrigi66@gmail.com>
2024-07-18 21:09:37 +0430

    try

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import logging
 import logging.handlers
 import os
-
 import requests
+import base64
+from bs4 import BeautifulSoup
+import smtplib
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -15,7 +17,7 @@ logger_file_handler = logging.handlers.RotatingFileHandler(
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger_file_handler.setFormatter(formatter)
 logger.addHandler(logger_file_handler)
-h = 0
+
 try:
     SOME_SECRET = os.environ["SOME_SECRET"]
     CK = os.environ["CK"]
@@ -30,12 +32,38 @@ except KeyError:
     SOME_SECRET = "Token not available!"
     CK = "Token not available!"
     CS = "Token not available!"
+    PRODUCT_URL = "Token not available!"
     BATCH_PRODUCT_URL = "Token not available!"
     MY_GMAIL = "Token not available!"
     MY_GMAIL_APP_PASS = "Token not available!"
+    RECEIVE_GMAIL = "Token not available!"
     PRICE_TARGET_URL = "Token not available!"
     #logger.info("Token not available!")
     #raise
+
+# create authorization header
+credentials = f"{CK}:{CS}"
+base64_credentials = base64.b64encode(credentials.encode("utf-8")).decode("utf-8")
+headers = {
+    "Authorization": f"Basic {base64_credentials}",
+    "User-Agent": "My User Agent"  # replace with a valid user agent string
+}
+
+param = {
+    "per_page": 100
+}
+
+try:
+    response = requests.get(url=PRODUCT_URL, headers=headers, params=param)
+    response.raise_for_status()
+    products = response.json()
+except Exception as error:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_GMAIL, password=MY_GMAIL_APP_PASS)
+        connection.sendmail(from_addr=MY_GMAIL,
+                        to_addrs=f"{RECEIVE_GMAIL}",
+                        msg=f"Subject:Woo Price update failed\n\nAccess to the products was fail. So, products price update failed. Error message: {error}")
 
 
 if __name__ == "__main__":
